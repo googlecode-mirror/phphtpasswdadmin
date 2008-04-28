4befd6a294e62df428651e7b3da4f0ebde22a2b9
hi <hi@charry.org>
2008-04-28 07:19:26 +0000

    initial import

---
+++ php_htpasswd_admin/index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>Update Passwd</title>
+<script>
+function ck()
+{
+	var u = document.getElementById("username");
+	var op = document.getElementById("old_passwd");
+	var np = document.getElementById("new_passwd");
+
+	if (u.value == '' || op.value == '' || np.value == '')
+	{
+		alert('Invalid Username or Password');
+		return false;
+	}
+
+	return true;
+}
+</script>
+</head>
+<body>
+
+<form action="update.php" name="form1" method="post" onsubmit="return ck();">
+<table>
+<tr>
+<td>Username</td>
+<td><input type="text" id="username" name="username"></td>
+</tr>
+<tr>
+<td>Old Passwd</td>
+<td><input type="password" id="old_passwd" name="old_passwd"></td>
+<tr>
+<td>New Passwd</td>
+<td><input type="password" id="new_passwd" name="new_passwd"></td>
+</tr>
+</table>
+
+<input type="submit" name="submit" value="Update Passwd" onclick="document.all.form1.submit();"/>
+</form>
+
+<br/>
+Your password will be synced to bumblebee 5 min later.
+</body>
+</html>
+
---
+++ php_htpasswd_admin/update.php
@@ -0,0 +1,83 @@
+<?php
+
+function c($plainpasswd, $salt) {
+    // $salt = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz0123456789"), 0, 8);
+
+    $len = strlen($plainpasswd);
+    $text = $plainpasswd.'$apr1$'.$salt;
+    $bin = pack("H32", md5($plainpasswd.$salt.$plainpasswd));
+    for($i = $len; $i > 0; $i -= 16) { $text .= substr($bin, 0, min(16, $i)); }
+    for($i = $len; $i > 0; $i >>= 1) { $text .= ($i & 1) ? chr(0) : $plainpasswd{0}; }
+    $bin = pack("H32", md5($text));
+    for($i = 0; $i < 1000; $i++) {
+        $new = ($i & 1) ? $plainpasswd : $bin;
+        if ($i % 3) $new .= $salt;
+        if ($i % 7) $new .= $plainpasswd;
+        $new .= ($i & 1) ? $bin : $plainpasswd;
+        $bin = pack("H32", md5($new));
+    }
+    for ($i = 0; $i < 5; $i++) {
+        $k = $i + 6;
+        $j = $i + 12;
+        if ($j == 16) $j = 5;
+        $tmp = $bin[$i].$bin[$k].$bin[$j].$tmp;
+    }
+    $tmp = chr(0).chr(0).$bin[11].$tmp;
+    $tmp = strtr(strrev(substr(base64_encode($tmp), 2)),
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+    "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    return "$"."apr1"."$".$salt."$".$tmp;
+}
+
+function update($user, $old_passwd, $new_passwd)
+{
+	$AUTH_USER_FILE = "d:/apps/repo/passwd.txt";
+
+	$lines = split("\n", file_get_contents($AUTH_USER_FILE));
+
+	for ($i=0; $i<count($lines); ++$i)
+	{
+		$tmp = split(":", $lines[$i]);
+		$_user = $tmp[0];
+		$_passwd = $tmp[1];
+
+		if ($_user == $user)
+		{
+			$tmp = explode("$", $lines[$i]);
+			$salt = $tmp[2];
+
+			if (c($old_passwd, $salt) == $_passwd)
+			{
+				// update
+				$lines[$i] = $user.":".c($new_passwd, $salt);
+				$fp = fopen($AUTH_USER_FILE, "w");
+				for ($j=0; $j<count($lines)-1; ++$j)
+				{
+					fwrite($fp, $lines[$j]."\n");
+				}
+				fwrite($fp, $lines[$j]);
+
+				fclose($fp);
+				
+				return "Passwd Updated";
+			}
+			else
+			{
+				return "Incorrect Password";
+			}
+		}
+	}
+	
+	return "User Not Found";
+}
+
+
+$username = $_POST['username'];
+$old_passwd = $_POST['old_passwd'];
+$new_passwd = $_POST['new_passwd'];
+
+$result = update($username, $old_passwd, $new_passwd);
+
+echo "<script>window.alert('".$result."');this.location.href='index.php'</script>";
+?>
+
